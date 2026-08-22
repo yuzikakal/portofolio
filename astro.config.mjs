4c353d0ab6b7ef3bc8719970a697825062e3b9f9
yuzikakal <yuzikayumecca@gmail.com>
2026-08-22 23:28:08 +0000

    header nav design update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from "astro-icon"
 import react from '@astrojs/react';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [icon(), react()]
+  integrations: [icon(), react(), alpinejs()]
 });
